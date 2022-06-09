1ca190d4ab6522bdb0032622e678534db27fe29f
Andrei Godlevskii <andrei.godlevskii@icloud.com>
2022-06-09 20:07:46 +0300

    Sprint 2 increment 6: File storage
    * Remove unsupported unit-test.

--- cmd/shortener/server_test.go
+++ cmd/shortener/server_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"go-url-shortener/internal/testhelp"
-	"reflect"
 	"testing"
 )
 
@@ -45,38 +43,6 @@ func Test_getServerAddress(t *testing.T) {
 			}
 
 			testhelp.RemoveEnvVar(addrKey)
-		})
-	}
-}
-
-func Test_getRepo(t *testing.T) {
-	tests := []struct {
-		name string
-		want string
-		args struct{ filename string }
-	}{
-		{
-			name: "Missing env variable",
-			want: "MemoRepo",
-		},
-		{
-			name: "Existing env variable",
-			args: struct{ filename string }{filename: "testfile"},
-			want: "FileRepo",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if tt.args.filename != "" {
-				testhelp.SetEnvVar(storageFileName, tt.args.filename)
-			}
-
-			got, _ := getRepo()
-			assert.Equal(t, tt.want, reflect.TypeOf(got).Name())
-
-			if tt.args.filename != "" {
-				testhelp.RemoveEnvVar(storageFileName)
-			}
 		})
 	}
 }
