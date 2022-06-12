435b15831cd8f354c6b46f6b21421c209c2184f4
Andrei Godlevskii <andrei.godlevskii@icloud.com>
2022-06-12 12:02:37 +0300

    Sprint 2 increment 8: Compression
    * Implement gzip response writer.
    * Implement custom compression middleware.
    * Compress the response starting from the specific length if it's supported.

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,13 +1,21 @@
 package handlers
 
 import (
+	"compress/gzip"
 	"github.com/go-chi/chi/v5"
+	"go-url-shortener/internal/respwriters"
 	"go-url-shortener/internal/storage"
+	"io"
 	"net/http"
+	"strconv"
+	"strings"
 )
+
+const compressFormat = "gzip"
 
 func NewShortenerRouter(db storage.Storager, baseURL string) *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(compress)
 
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", GetHomePage)
@@ -24,4 +32,35 @@ func NewShortenerRouter(db storage.Storager, baseURL string) *chi.Mux {
 	})
 
 	return r
+}
+
+func compress(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		enc := w.Header().Get("Accept-Encoding")
+		size := w.Header().Get("Content-Length")
+		sizeInt, err := strconv.Atoi(size)
+		if err != nil {
+			sizeInt = 0
+		}
+
+		if !strings.Contains(enc, compressFormat) || sizeInt < 1400 {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(w, gzip.BestCompression)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		gzWriter := respwriters.GzipWriter{
+			ResponseWriter: w,
+			Writer:         gz,
+		}
+
+		w.Header().Set("Content-Encoding", compressFormat)
+		next.ServeHTTP(gzWriter, r)
+	})
 }
