5c9b9e886c5c1d2103c6c76e1ceb5608f8320c33
Andrei Godlevskii <andrei.godlevskii@icloud.com>
2022-06-12 12:08:38 +0300

    Sprint 2 increment 8: Compression
    * Log URL validation error.

--- internal/validators/url_validator.go
+++ internal/validators/url_validator.go
@@ -1,8 +1,12 @@
 package validators
 
-import "net/url"
+import (
+	log "github.com/sirupsen/logrus"
+	"net/url"
+)
 
 func IsURLStringValid(rawURL string) bool {
 	_, err := url.ParseRequestURI(rawURL)
+	log.Error(err)
 	return err == nil
 }
